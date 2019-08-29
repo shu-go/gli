87f93e2ec4a1945b5d0e6755758fd236ffea1cc7
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2019-08-29 13:40:59 +0900

    add comments

--- user_opt_type.go
+++ user_opt_type.go
@@ -20,6 +20,9 @@ type Parsable interface {
 // see https://golang.org/pkg/time/#ParseDuration
 type Date time.Time
 
+// Parse parses string str and stores it as gli.Date (time.Time)
+//
+// examples: "2019-12-31", "2019/12/31"
 func (d *Date) Parse(str string) error {
 	tm, err := time.Parse("2006-01-02", str)
 	if err != nil {
@@ -34,6 +37,7 @@ func (d *Date) Parse(str string) error {
 	return nil
 }
 
+// Time returns gli.Date as time.Time
 func (d Date) Time() time.Time {
 	return time.Time(d)
 }
@@ -45,6 +49,9 @@ func (d Date) Time() time.Time {
 // see https://golang.org/pkg/time/#ParseDuration
 type Duration time.Duration
 
+// Parse parses string str and stores it as gli.Duration (time.Time)
+//
+// examples: "10m" ten minutes, "1s" one second
 func (d *Duration) Parse(str string) error {
 	dur, err := time.ParseDuration(str)
 	if err != nil {
@@ -56,6 +63,7 @@ func (d *Duration) Parse(str string) error {
 	return nil
 }
 
+// Time returns gli.Duration as time.Time
 func (d Duration) Duration() time.Duration {
 	return time.Duration(d)
 }
@@ -67,6 +75,9 @@ type Range struct {
 	Min, Max string
 }
 
+// Parse parses string str and stores it as gli.Range (Min, Max string)
+//
+// examples: "A:Z", "1:100", ":0"
 func (r *Range) Parse(str string) error {
 	if !strings.Contains(str, ":") {
 		return fmt.Errorf("range format is min:max")
@@ -85,6 +96,9 @@ func (r *Range) Parse(str string) error {
 // StrList is a parsable type of ["s", "t", "r"]
 type StrList []string
 
+// Parse parses string str and stores it as gli.StrList ([]string)
+//
+// examples: "a,b,c"
 func (l *StrList) Parse(str string) error {
 	*l = (*l)[:0]
 	list := strings.Split(str, ",")
@@ -95,6 +109,7 @@ func (l *StrList) Parse(str string) error {
 	return nil
 }
 
+// Contains tests string s is in the gli.StrList
 func (l StrList) Contains(s string) bool {
 	for _, v := range l {
 		if v == s {
@@ -109,6 +124,9 @@ func (l StrList) Contains(s string) bool {
 // IntList is a parsable type of [1, 2, 3]
 type IntList []int
 
+// Parse parses string str and stores it as gli.IntList ([]int)
+//
+// examples: "1,2,3"
 func (l *IntList) Parse(str string) error {
 	*l = (*l)[:0]
 	list := strings.Split(str, ",")
@@ -124,6 +142,7 @@ func (l *IntList) Parse(str string) error {
 	return nil
 }
 
+// Contains tests string s is in the gli.IntList
 func (l IntList) Contains(i int) bool {
 	for _, v := range l {
 		if v == i {
@@ -138,6 +157,9 @@ func (l IntList) Contains(i int) bool {
 // Map is a parsable type of key=value or key:value pair.
 type Map map[string]string
 
+// Parse parses string str and stores it as gli.Map (map[string]string)
+//
+// examples: "key1:value1,key2:value2"
 func (m *Map) Parse(str string) error {
 	if *m == nil {
 		*m = make(map[string]string)
